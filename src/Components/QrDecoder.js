495d7d02aafa98b011c5dc41cb38f190c8595a45
Dideoluwa Oni <darasimidideoluwa@gmail.com>
2022-08-22 06:34:01 +0100

    commit

--- src/Components/QrDecoder.js
+++ src/Components/QrDecoder.js
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './QrDecoder.module.css'
 import QrDecoderCamera from './QrDecoderCamera';
 import QrDecoderFile from './QrDecoderFile';
 
 
 function QrDecoder() {
-    
+    let [isChangedMode, setIsChangedMode] = useState(false)
+
+    let uploadFileHandler = () => {
+        setIsChangedMode(false)
+    }
+
+    let uploadScanHandler = () => {
+        setIsChangedMode(true)
+    }
+
     return (
         <div className={styles.body}>
             <div className={styles.header}>
                 <h1>QrDecoder</h1>
             </div>
-           {/* <QrDecoderCamera /> */}
-           <QrDecoderFile />
+            <div className={styles.nav}>
+                <div
+                    onClick={uploadFileHandler}
+                >
+                    <h2>Upload File</h2>
+                </div>
+
+                <div
+                    onClick={uploadScanHandler}
+                >
+                    <h2>Scan Code</h2>
+                </div>
+            </div>
+            {isChangedMode && <QrDecoderCamera />}
+            {!isChangedMode && <QrDecoderFile />}
         </div>
     )
 }
--- src/Components/QrDecoderFile.js
+++ src/Components/QrDecoderFile.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, { useState , useRef } from 'react'
 import styles from './QrDecoderFile.module.css'
+import placeholder from '../Assets/placeholder.jpeg'
+// import QrReader from 'react-qr-reader';
+import axios from 'axios'
 
 function QrDecoderFile() {
-  return (
-    <div>QrDecoderFile</div>
-  )
+    // let [QrImg, setQrImg] = useState([])
+    let [QrImgShow, setQrImgShow] = useState(false)
+    const [image, setImage] = useState([])
+    let [data, setData] = useState([])
+    let [imgName, setImgName] = useState('')
+    // let photoRef = useRef()
+
+    let images = QrImgShow ? image : placeholder
+
+
+    let formSubmitHandler = (e) => {
+        e.preventDefault()
+        if (image.length === 0) {
+            setQrImgShow(false)
+            return
+        }
+        let url = `http://api.qrserver.com/v1/read-qr-code/?fileurl=${imgName}`
+        axios.get(url, {
+            headers: {"Access-Control-Allow-Origin": "*"} 
+        }).then((res) => {
+            console.log(res.data.symbol.data)
+            setData(res.data.symbol.data)
+        })
+    }
+
+    const fileChangeHandler = (e) => {
+        if (e.target.files && e.target.files[0]) {
+            setQrImgShow(true)
+            setImgName(e.target.files[0].name)
+            setImage(URL.createObjectURL(e.target.files[0]));
+        }
+    }
+    return (
+        <div className={styles.body}>
+            <div className={styles.header}>
+                <h1>Upload File</h1>
+            </div>
+            <div className={styles.body_form}>
+                <div className={styles.body_form_2}>
+                    <form
+                        onSubmit={formSubmitHandler}>
+                        <div className={styles.inputField}>
+                            <input
+                            //    ref={photoRef}
+                                accept="image/*"
+                                name='file'
+                                onChange={fileChangeHandler}
+                                type='file'
+                            ></input>
+                        </div>
+                        <button>Decode QR code</button>
+                    </form>
+                </div>
+                <div className={styles.body2}>
+
+                    <div className={styles.qr_code}>
+                        <img src={images} alt='Qr code' />
+                    </div>
+                </div>
+                <h1>{data}</h1>
+            </div>
+        </div>
+    )
 }
 
 export default QrDecoderFile
